46456337cf92d90f380336727e55114ac3d02748
Arafat Patwary <farabipsk@gmail.com>
2024-11-16 10:42:02 +0600

    Complete form

--- src/components/form/StudentForm.jsx
+++ src/components/form/StudentForm.jsx
@@ -1,36 +1,68 @@
 import React, { useState } from 'react'
 
-const  StudentForm =() => {
+const StudentForm = () => {
     //? Manage state //
     const [studentName, setStudentName] = useState("");
     const [age, setAge] = useState("");
-    const [course, setCourse] = useState("science");
-    const [content, setContent] = useState("");
+    const [course, setCourse] = useState("Science");
+    const [content, setContent] = useState(false);
 
     //? Manage handle //
-    const handleNameChange =(e)=>{
-        console.log(e.target.value);
+    const handleNameChange = (e) => {
+        setStudentName(e.target.value);
     }
-    const handleAgeChange =(e)=>{}
-    const handleCourseChange =(e)=>{}
-    const handleContentChange =(e)=>{} 
+    const handleAgeChange = (e) => {
+        setAge(e.target.value);
+    }
+    const handleCourseChange = (e) => {
+        setCourse(e.target.value);
+    }
+    const handleContentChange = (e) => {
+        setContent(e.target.checked);
+    }
 
     //! Handle submit //
-    const handleSubmit =(e) =>{
-        
-
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log({
+            studentName,
+            age,
+            course,
+            content,
+        })
+    
     }
 
-  return (
-    <form action="">
-        <h2>Student Registration Form</h2>
-        <div>
-            <label>Name:</label>
-            <input type="text" value={studentName}
-            onChange={handleNameChange} />
-        </div>
-    </form>
-  )
+    return (
+        <form onSubmit={handleSubmit}>
+            <h2>Student Registration Form</h2>
+            <div>
+                <label>Name:</label>
+                <input type="text" value={studentName}
+                    onChange={handleNameChange} />
+            </div>
+            <div>
+                <label>Age:</label>
+                <input type="number" value={age}
+                    onChange={handleAgeChange} />
+            </div>
+            <div>
+                <label>Course:</label>
+                <select value={course} onChange={handleCourseChange}>
+                    <option value={'Math'}>Math</option>
+                    <option value={'Physics'}>Physics</option>
+                    <option value={'Chemistry'}>Chemistry</option>
+                    <option value={'Biology'}>Biology</option>
+                </select>
+            </div>
+            <div>
+                <label>Content:</label>
+                <input type="checkbox" checked={content}
+                    onChange={handleContentChange} />
+            </div>
+            <button type='submit'>Register</button>
+        </form>
+    )
 }
 
 export default StudentForm
