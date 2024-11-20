9a3642ae189875d77463329bb2c22e79ec2f063d
Arafat Patwary <farabipsk@gmail.com>
2024-11-20 16:32:00 +0600

    Add React Router

--- src/App.js
+++ src/App.js
@@ -5,19 +5,30 @@ import Task from './components/taskApp/Task';
 import Https from './components/http/HttpApi';
 import StudentForm from './components/form/StudentForm';
 import CounterApp from './components/counterApp/CounterApp';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import About from './components/reactRouter/About';
+import Post from './components/reactRouter/Post';
 
 
 
 function App() {
   return (
-    <div className="App">
-      {/* <Counter/> */}
-      <CounterApp/>
-      <Task/>
-      {/* <Https/> */}
-      <StudentForm/>
+    // <div className="App">
+    //   {/* <Counter/> */}
+    //   <CounterApp/>
+    //   <Task/>
+    //   {/* <Https/> */}
+    //   <StudentForm/>
       
-    </div>
+    // </div>
+  <BrowserRouter>
+  <Routes>
+      <Route path="/" element={<About/>} />
+      <Route path="/post" element={<Post/>} />
+
+     
+  </Routes>
+  </BrowserRouter>
   );
 }
 
